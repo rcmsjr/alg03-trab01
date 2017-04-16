c70300f9c7a9c0d544d43c11971f8964899393fa
Thiago Silva <thiago0112@gmail.com>
2017-04-16 09:06:32 -0300

    Add matrix file reader and console util class

---
+++ src/file/MatrixFileReader.java
@@ -0,0 +1,77 @@
+package file;
+
+import java.io.*;
+
+/**
+ * Leitor de arquivos textos que possuam matriz
+ */
+class MatrixFileReader {
+    private final String filePath;
+
+    public MatrixFileReader(String filePath) {
+        this.filePath = filePath;
+    }
+
+    /**
+     * Le um arquivo texto e transforma em matriz
+     *
+     * @return matriz
+     */
+    public int[][] read() {
+        File file = new File(this.filePath);
+        FileReader reader = null;
+        BufferedReader bufferedReader = null;
+        int matrix[][] = null;
+
+        try {
+            reader = new FileReader(file);
+            bufferedReader = new BufferedReader(reader);
+            matrix = this.createMatrix(bufferedReader);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+
+                if (bufferedReader != null) {
+                    bufferedReader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return matrix;
+    }
+
+    /**
+     * Cria uma matriz atravez do leitor de buffer de um arquivo
+     * @param bufferedReader
+     * @throws IOException
+     * @return matriz
+     */
+    private int[][] createMatrix(BufferedReader bufferedReader) throws IOException {
+        int matrix[][];
+        String currentLine;
+        String[] matrixConfig;
+
+        currentLine = bufferedReader.readLine();
+        matrixConfig = currentLine.split(" ");
+        matrix = new int[Integer.parseInt(matrixConfig[1])][Integer.parseInt(matrixConfig[0])];
+
+        int i = 0;
+
+        while ((currentLine = bufferedReader.readLine()) != null) {
+            for (int x = 0, len = currentLine.length(); x < len; x++) {
+                matrix[i][x] = Integer.parseInt(String.valueOf(currentLine.charAt(x)));
+            }
+
+            i++;
+        }
+
+        return matrix;
+
+    }
+}
---
+++ src/util/Console.java
@@ -0,0 +1,51 @@
+package util;
+
+import java.util.Scanner;
+
+/**
+ * Console util methods
+ *
+ */
+class Console {
+
+    private static final Scanner keyboard = new Scanner(System.in);
+
+    /**
+     * Solicita uma string do teclado e a le
+     *
+     * @param prompt
+     *
+     * @return String informada
+     */
+    public static String readString(String prompt) {
+        Console.printLn(prompt);
+
+        return keyboard.next();
+    }
+
+    /**
+     * Imprime uma string
+     *
+     * @param s
+     */
+    public static void print(String s) {
+        System.out.print(s);
+    }
+
+    /**
+     * Imprime uma linha vazia
+     *
+     */
+    public static void printLn() {
+        System.out.println();
+    }
+
+    /**
+     * Imprime uma string em uma nova linha
+     * @param s
+     */
+    private static void printLn(String s)
+    {
+        System.out.println(s);
+    }
+}
